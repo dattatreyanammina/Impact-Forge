31d8ad921f31caff060728683a8a6f5cefa49666
Dattatreya <dattatreya_nammina@srmap.edu.in>
2026-04-28 11:45:54 +0530

    fix firebase init (use web config)

--- lib/core/firebase/firebase_bootstrap.dart
+++ lib/core/firebase/firebase_bootstrap.dart
@@ -1,20 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
-import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import '../../firebase_options.dart';
 
 class FirebaseBootstrap {
   static Future<void> initialize() async {
-    if (Firebase.apps.isNotEmpty) return;
-
-    if (kIsWeb) {
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.web,
-      );
-    } else {
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-    }
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.web,
+    );
   }
 }
