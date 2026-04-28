85ce4faf3a76e927a2853e18edf460866323c85a
Dattatreya <dattatreya_nammina@srmap.edu.in>
2026-04-28 12:19:03 +0530

    fix firebase bootstrap final

--- lib/core/firebase/firebase_bootstrap.dart
+++ lib/core/firebase/firebase_bootstrap.dart
@@ -1,10 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
-import '../../firebase_options.dart';
 
 class FirebaseBootstrap {
   static Future<void> initialize() async {
     await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDMnDA_lUVXl8oqmmjTZAyNzNYSGKNmc8s",
+        appId: "1:937705200109:web:8265f895c9ec2f7a46efe4",
+        messagingSenderId: "937705200109",
+        projectId: "hack-a10bb",
+        authDomain: "hack-a10bb.firebaseapp.com",
+        storageBucket: "hack-a10bb.appspot.com",
+      ),
     );
+
+    print("🔥 FIREBASE WORKING");
   }
 }
