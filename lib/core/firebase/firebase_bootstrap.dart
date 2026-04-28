712dc649efb394240f5dca5e42be56961fdda8f4
Dattatreya <dattatreya_nammina@srmap.edu.in>
2026-04-28 11:43:32 +0530

    fix firebase init (bootstrap)

--- lib/core/firebase/firebase_bootstrap.dart
+++ lib/core/firebase/firebase_bootstrap.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import '../../firebase_options.dart';
 
 class FirebaseBootstrap {
-  const FirebaseBootstrap._();
-
   static Future<void> initialize() async {
     if (Firebase.apps.isNotEmpty) return;
 
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+    if (kIsWeb) {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.web,
+      );
+    } else {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+    }
   }
 }
